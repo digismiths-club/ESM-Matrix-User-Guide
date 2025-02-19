31c8da55f54ecf078f32955280c05e36e1b0accf
Akriti <akritisemwal11@gmail.com>
2025-02-19 13:35:44 +0530

    changes occured

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -11,11 +11,11 @@ function  About() {
           <div className='xl:mt-4 mt-2 font-light text-xs xl:text-base'>
             An Eisenhower Matrix Task Manager to <b>Do, Schedule, Delegate, or Defer</b> tasks. Prioritize efficiently, set deadlines, assign responsibilities, and track unfinished work. Boost productivity with an intuitive interface, ensuring better time management and decision-making for seamless task execution and organization.
           </div>
-          <form action="https://marketplace.zoho.com/app/crm/google-address-autocomplete-widget-for-zoho-crm" target="_blank" >
+          {/* <form action="https://marketplace.zoho.com/app/crm/google-address-autocomplete-widget-for-zoho-crm" target="_blank" >
             <button type='submit' className='mt-7 bg-blue-400 py-2 px-3 rounded-md text-white hover:bg-blue-500 text-sm'>
               Install Now
             </button>
-          </form>
+          </form> */}
         </div>
         <img src="/ESM-Matrix-User-Guide/logo.svg" className='h-44 w-44 xl:h-96 xl:w-96' alt='logo' width={500} height={500}/>
       </div>
