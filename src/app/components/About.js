385dae8669e75c5f479fbc95438dfb602d03998f
Puneet Chandhok <punu13@gmail.com>
2025-02-21 04:23:48 +0530

    change in intro text

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -9,7 +9,7 @@ function  About() {
           <div className='xl:text-4xl xl:mt-2 font-semibold xl:font-normal'>Eisenhower Matrix For Zoho CRM</div>
           <div className='xl:mt-2 mt-1 text-sm xl:text-lg'>A Free Tool by <b>Digismiths</b></div>
           <div className='xl:mt-4 mt-2 font-light text-xs xl:text-base'>
-            An Eisenhower Matrix Task Manager to <b>Do, Schedule, Delegate, or Defer</b> tasks. Prioritize efficiently, set deadlines, assign responsibilities, and track unfinished work. Boost productivity with an intuitive interface, ensuring better time management and decision-making for seamless task execution and organization.
+            An <strong>Eisenhower Matrix Task Manager</strong> is a powerful task management tool that seamlessly integrates with Zoho CRM, designed to help users prioritize their work effectively using the Eisenhower Matrix. This tool provides a visual, interactive, and efficient approach to task organization, boosting productivity and enabling users to focus on what truly matters.
           </div>
           {/* <form action="https://marketplace.zoho.com/app/crm/google-address-autocomplete-widget-for-zoho-crm" target="_blank" >
             <button type='submit' className='mt-7 bg-blue-400 py-2 px-3 rounded-md text-white hover:bg-blue-500 text-sm'>
